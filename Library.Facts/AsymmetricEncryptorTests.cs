112f0018a6d29e1e62d2b6255c217d8d13606f4e
Simon Neidhart <simon.neidhart@hotmail.com>
2024-09-02 14:58:21 +0200

    add exercises for asymmetric encryption

---
+++ Library.Facts/AsymmetricEncryptorTests.cs
@@ -0,0 +1,26 @@
+﻿using EncryptionDecryption;
+
+namespace Library.Facts;
+
+using FluentAssertions;
+
+public class AsymmetricEncryptorTests
+{
+    private const string Plaintext = "Don't tell anybody: I'm encrypted";
+    private readonly AsymmetricEncryptor testee;
+
+    public AsymmetricEncryptorTests()
+    {
+        this.testee = new AsymmetricEncryptor();
+    }
+    
+    [Fact]
+    private void WhenEncryptedAndDecryptedShouldBeTheSameString()
+    {
+        var encrypted = this.testee.Encrypt(Plaintext);
+        encrypted.Should().NotBe(Plaintext, "cleartext needs to be encrypted");
+
+        var plaintext = this.testee.Decrypt(encrypted);
+        plaintext.Should().Be(Plaintext);
+    }
+}
---
+++ Library/AsymmetricEncryptor.cs
@@ -0,0 +1,28 @@
+﻿namespace EncryptionDecryption;
+
+/// <summary>
+/// This class should be the place where RSA is used to encrypt and decrypt strings
+/// </summary>
+public class AsymmetricEncryptor
+{
+    public AsymmetricEncryptor()
+    {
+        // initialize RSA
+    }
+
+    public string Encrypt(string plaintext)
+    {
+        // 1. get the bytes from the encoded plaintext.
+        // 2. use RSA to encrypt the bytes from the encoded plaintext. This will give you a byte array.
+        // 3. convert byte array to base64 string and return.
+        return null;
+    }
+
+    public string Decrypt(string encryptedBase64)
+    {
+        // 1. convert the encrypted base64 string to a byte array.
+        // 2. use RSA to decrypt this byte array.
+        // 3. Encode this byte array to a UTF8 string and return.
+        return null;
+    }
+}
